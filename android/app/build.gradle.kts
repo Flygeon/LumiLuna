b9eccd48ee32f6c2c1a37d37ee236ab1788a37aa
Flygeon <v2972632024@163.com>
2026-07-16 16:25:05 +0800

    build(android): 更新安卓应用包名与编译SDK版本
    将应用包名从com.example.lumiluna改为com.flygeon.lumiluna，同时将compileSdk和targetSdk版本统一升级到36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.lumiluna"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.flygeon.lumiluna"
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -16,11 +16,9 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.lumiluna"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.flygeon.lumiluna"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
